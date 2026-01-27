f84e66fe56f4a3ea82e5c8d53dd23807d197a445
samiadham12 <102438847+samiadham12@users.noreply.github.com>
2026-01-27 17:37:03 +0200

    Refactor PixelScripts for Improved TikTok Pixel Integration
    - Updated the PixelScripts component to enhance the TikTok pixel loading logic, ensuring that the pixel is pushed correctly to the tracking queue.
    - Modified the script to utilize a more efficient method for setting and deferring pixel methods, improving performance and reliability.
    - These changes contribute to a more robust analytics framework, ensuring accurate tracking of user interactions and data collection.
    
    This update builds on previous enhancements to pixel management, further refining the tracking capabilities within the application.

--- components/tracking/PixelScripts.tsx
+++ components/tracking/PixelScripts.tsx
@@ -47,10 +47,10 @@ export default function PixelScripts({ tenantId }: PixelScriptsProps) {
                 dangerouslySetInnerHTML={{
                   __html: `
                     !function (w, d, t) {
-                      w.TiktokAnalyticsObject=t;var ttq=w[t]=w[t]||[];ttq.methods=["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie"],ttq.setAndDefer=function(t,e){t.align=2,ttq.push([t].concat(Array.prototype.slice.call(e,0)))};for(var i=0;i<ttq.methods.length;i++)ttq.setAndDefer(ttq,ttq.methods[i]);ttq.instance=function(t){for(var e=ttq.methods[i],n=0;n<ttq.methods.length;n++)ttq.setAndDefer(e,ttq.methods[n]);return e},ttq.load=function(e,n){var i="https://analytics.tiktok.com/i18n/pixel/events.js";ttq._i=ttq._i||{},ttq._i[e]=[],ttq._i[e]._u=i,ttq._t=ttq._t||{},ttq._t[e]=+new Date,ttq._o=ttq._o||{},ttq._o[e]=n||{};var o=document.createElement("script");o.type="text/javascript",o.async=!0,o.src=i+"?sdkid="+e+"&lib="+t;var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(o,a)};
+                      w.TiktokAnalyticsObject=t;var ttq=w[t]=w[t]||[];ttq.methods=["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie"],ttq.setAndDefer=function(t,e){t[e[0]]=function(){t.push([e[0]].concat(Array.prototype.slice.call(arguments,0)))}};for(var i=0;i<ttq.methods.length;i++)ttq.setAndDefer(ttq,ttq.methods[i]);ttq.load=function(e,n){var i="https://analytics.tiktok.com/i18n/pixel/events.js";ttq._i=ttq._i||{},ttq._i[e]=[],ttq._i[e]._u=i,ttq._t=ttq._t||{},ttq._t[e]=+new Date,ttq._o=ttq._o||{},ttq._o[e]=n||{};var o=document.createElement("script");o.type="text/javascript",o.async=!0,o.src=i+"?sdkid="+e+"&lib="+t;var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(o,a)};
                     
                       ttq.load('${pixel.externalId}');
-                      ttq.ready(function(){ttq.page();});
+                      ttq.push(['page']);
                     }(window, document, 'ttq');
                   `,
                 }}
