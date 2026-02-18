b77ad6feb583bd4295226d031d1fef8e64d519dd
samiadham12 <102438847+samiadham12@users.noreply.github.com>
2026-02-18 18:42:34 +0200

    Refactor: Remove unused component functions imports from editor store
    This commit cleans up the editor store by removing obsolete imports of component functions that are no longer utilized. This refactoring helps streamline the codebase and improves maintainability by eliminating unnecessary dependencies.
    
    Files modified:
    - context/editorStore/store/index.ts

--- context/editorStore/store/index.ts
+++ context/editorStore/store/index.ts
@@ -17,53 +17,6 @@ import { createPageActions } from "./actions/pageActions";
 import { createComponentActions } from "./actions/componentActions";
 import { createDatabaseActions } from "./actions/databaseActions";
 
-// Import component functions for use in actions
-import { heroFunctions } from "../../editorStoreFunctions/heroFunctions";
-import { headerFunctions } from "../../editorStoreFunctions/headerFunctions";
-import { footerFunctions } from "../../editorStoreFunctions/footerFunctions";
-import { halfTextHalfImageFunctions } from "../../editorStoreFunctions/halfTextHalfImageFunctions";
-import { sideBySideFunctions } from "../../editorStoreFunctions/sideBySideFunctions";
-import { propertySliderFunctions } from "../../editorStoreFunctions/propertySliderFunctions";
-import { ctaValuationFunctions } from "../../editorStoreFunctions/ctaValuationFunctions";
-import { stepsSectionFunctions } from "../../editorStoreFunctions/stepsSectionFunctions";
-import { testimonialsFunctions } from "../../editorStoreFunctions/testimonialsFunctions";
-import { logosTickerFunctions } from "../../editorStoreFunctions/logosTickerFunctions";
-import { propertiesShowcaseFunctions } from "../../editorStoreFunctions/propertiesShowcaseFunctions";
-import { card4Functions } from "../../editorStoreFunctions/card4Functions";
-import { card5Functions } from "../../editorStoreFunctions/card5Functions";
-import { partnersFunctions } from "../../editorStoreFunctions/partnersFunctions";
-import { whyChooseUsFunctions } from "../../editorStoreFunctions/whyChooseUsFunctions";
-import { contactMapSectionFunctions } from "../../editorStoreFunctions/contactMapSectionFunctions";
-import { gridFunctions } from "../../editorStoreFunctions/gridFunctions";
-import { filterButtonsFunctions } from "../../editorStoreFunctions/filterButtonsFunctions";
-import { propertyFilterFunctions } from "../../editorStoreFunctions/propertyFilterFunctions";
-import { mapSectionFunctions } from "../../editorStoreFunctions/mapSectionFunctions";
-import { contactCardsFunctions } from "../../editorStoreFunctions/contactCardsFunctions";
-import { contactFormSectionFunctions } from "../../editorStoreFunctions/contactFormSectionFunctions";
-import { applicationFormFunctions } from "../../editorStoreFunctions/applicationFormFunctions";
-import { jobFormFunctions } from "../../editorStoreFunctions/jobFormFunctions";
-import { inputsFunctions } from "../../editorStoreFunctions/inputsFunctions";
-import { inputs2Functions } from "../../editorStoreFunctions/inputs2Functions";
-import { imageTextFunctions } from "../../editorStoreFunctions/imageTextFunctions";
-import { contactUsHomePageFunctions } from "../../editorStoreFunctions/contactUsHomePageFunctions";
-import { blogsSectionsFunctions } from "../../editorStoreFunctions/blogsSectionsFunctions";
-import { blogCardFunctions } from "../../editorStoreFunctions/blogCardFunctions";
-import { responsiveImageFunctions } from "../../editorStoreFunctions/responsiveImageFunctions";
-import { titleFunctions } from "../../editorStoreFunctions/titleFunctions";
-import { photosGridFunctions } from "../../editorStoreFunctions/photosGridFunctions";
-import { videoFunctions } from "../../editorStoreFunctions/videoFunctions";
-import { projectDetailsFunctions } from "../../editorStoreFunctions/projectDetailsFunctions";
-import { propertyDetailFunctions as propertyDetailFunctions } from "../../editorStoreFunctions/propertyDetailFunctions";
-import { blogDetailsFunctions } from "../../editorStoreFunctions/blogDetailsFunctions";
-import { createDefaultData } from "../../editorStoreFunctions/types";
-import {
-  logBefore,
-  logAfter,
-  logBeforeAfter,
-  logDuring,
-} from "@/lib/fileLogger";
-import type { ComponentInstanceWithPosition } from "../types/types";
-
 export const useEditorStore = create<EditorStore>((set, get) => {
   const initialState = getInitialState();
 
